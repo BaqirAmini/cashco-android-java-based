c5c4cebda0da1b4db97864aa20ec0525270d684a
= <=>
2019-08-08 11:31:03 +0430

    Switch Username from Firstname to login

--- app/src/main/java/com/xamuor/cashco/Utilities/Routes.java
+++ app/src/main/java/com/xamuor/cashco/Utilities/Routes.java
@@ -2,13 +2,13 @@ package com.xamuor.cashco.Utilities;
 
 public class Routes {
 //    Remote-server...
-    private static String IMG_PATH = "http://pos.xamuor.com/uploads/";
+    private static String IMG_PATH = "http://cashco.xamuor.com/uploads/";
 //    Virtual-device IP
 //    private static String IMG_PATH = "http://192.168.56.1:8000/uploads/";
 //    private static String IMG_PATH = "http://192.168.133.2:8000/uploads/";
     public static String setUrl(String route) {
         //    Remote-server...
-        String url = "http://pos.xamuor.com/api/" + route;
+        String url = "http://cashco.xamuor.com/api/" + route;
 //        String url = "http://192.168.56.1:8000/api/" + route;
 //        Virtual-device IP
         return url;
