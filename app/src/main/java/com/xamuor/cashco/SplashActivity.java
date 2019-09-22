6e4c11ed3658c2a6e5eaf9950911d1b436f4d490
= <=>
2019-09-22 11:41:37 +0430

    Add Splash-screen

---
+++ app/src/main/java/com/xamuor/cashco/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.xamuor.cashco;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.xamuor.cashco.cashco.R;
+
+import java.util.Objects;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView splashLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+//      Hide actinoBar
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+//        Initialize imageview spashLogo
+        splashLogo = findViewById(R.id.img_logo);
+
+//        Initiate animation
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.transition);
+        splashLogo.startAnimation(animation);
+        final Intent intent = new Intent(this, LoginActivity.class);
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
