55bfeaef0784620fe7bb5eac59f611c9ea372e9b
= <=>
2019-10-10 13:48:38 +0430

    Optimize design of custom dialog inside invoice

--- app/src/main/java/com/xamuor/cashco/Adapters/InvoiceAdapter.java
+++ app/src/main/java/com/xamuor/cashco/Adapters/InvoiceAdapter.java
@@ -16,6 +16,7 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -44,6 +45,7 @@ public class InvoiceAdapter extends ArrayAdapter<InvoiceDataModal> {
     @Override
     public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         if (convertView == null) {
+            final InvoiceDataModal product = getItem(position);
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.invoice_datamodal_layout, null, true);
 
@@ -51,52 +53,56 @@ public class InvoiceAdapter extends ArrayAdapter<InvoiceDataModal> {
             convertView.setOnLongClickListener(new View.OnLongClickListener() {
                 @Override
                 public boolean onLongClick(View view) {
-                    openInvoiceDialog();
+                    openInvoiceDialog(product.getProductName(), product.getProductQty(), product.getProductPrice(), product.getProductSubtotal());
                     return true;
                 }
             });
 
+
+            //        Widgets out of invoice data modal (fragment_invoice.xml)
+            final TextView txtSubtotal = ((Activity)context).findViewById(R.id.txt_val_subtotal);
+            final TextView txtTax = ((Activity)context).findViewById(R.id.txt_val_tax);
+            final TextView txtTotal = ((Activity)context).findViewById(R.id.txt_val_total);
+            final ListView listView = ((Activity)context).findViewById(R.id.list_invoice_content);
+            Button btnPayInvoice = ((Activity)context).findViewById(R.id.btn_pay_invoice);
+
+
+            btnPayInvoice.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+//                double subTotal = Double.parseDouble(txtSubtotal.getText().toString());
+                    onPay();
+                }
+            });
+
+            //        Initiate widgets of invoice-data-modal
+            TextView txtItemName = convertView.findViewById(R.id.txt_item_name);
+            TextView txtItemQty = convertView.findViewById(R.id.txt_item_qty);
+            TextView txtItemPrice = convertView.findViewById(R.id.txt_item_price);
+            TextView txtItemSubtotal = convertView.findViewById(R.id.txt_item_subtotal);
+
+
+
+//        define values inside widgets from invoiceDataModal
+            if (product != null) {
+                txtItemQty.setText(product.getProductQty()+"");
+            }
+            if (product != null) {
+                txtItemName.setText(product.getProductName());
+            }
+            if (product != null) {
+                txtItemPrice.setText(product.getProductPrice() + "");
+            }
+            if (product != null) {
+                txtItemSubtotal.setText(product.getProductSubtotal() + "");
+            }
         }
 
 
         remove(position);
 
-        InvoiceDataModal product = getItem(position);
-//        Initiate widgets of invoice-data-modal
-        TextView txtItemName = convertView.findViewById(R.id.txt_item_name);
-        TextView txtItemQty = convertView.findViewById(R.id.txt_item_qty);
-        TextView txtItemPrice = convertView.findViewById(R.id.txt_item_price);
-        TextView txtItemSubtotal = convertView.findViewById(R.id.txt_item_subtotal);
-
-//        Widgets out of invoice data modal (fragment_invoice.xml)
-        final TextView txtSubtotal = ((Activity)context).findViewById(R.id.txt_val_subtotal);
-        final TextView txtTax = ((Activity)context).findViewById(R.id.txt_val_tax);
-        final TextView txtTotal = ((Activity)context).findViewById(R.id.txt_val_total);
-        final ListView listView = ((Activity)context).findViewById(R.id.list_invoice_content);
-        Button btnPayInvoice = ((Activity)context).findViewById(R.id.btn_pay_invoice);
 
 
-        btnPayInvoice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                double subTotal = Double.parseDouble(txtSubtotal.getText().toString());
-                onPay();
-            }
-        });
-
-//        define values inside widgets from invoiceDataModal
-        if (product != null) {
-            txtItemQty.setText(product.getProductQty()+"");
-        }
-        if (product != null) {
-            txtItemName.setText(product.getProductName());
-        }
-        if (product != null) {
-            txtItemPrice.setText(product.getProductPrice() + "");
-        }
-        if (product != null) {
-            txtItemSubtotal.setText(product.getProductSubtotal() + "");
-        }
 
        /* final SwipeToDismissTouchListener<ListViewAdapter> touchListener = new SwipeToDismissTouchListener<>(new ListViewAdapter(invoiceListView),
                 new SwipeToDismissTouchListener.DismissCallbacks<ListViewAdapter>() {
@@ -181,11 +187,38 @@ public class InvoiceAdapter extends ArrayAdapter<InvoiceDataModal> {
         });
     }*/
 
-    private void openInvoiceDialog() {
-        Dialog d = new Dialog(context);
+    private void openInvoiceDialog(String product, int qty, double price, double total) {
+        TextView txtProduct;
+        EditText editQty, editPrice, editDiscount, editTotal;
+        Button btnMinus, btnPlus, btnDone, btnCancel;
+        final Dialog d = new Dialog(context);
         d.requestWindowFeature(Window.FEATURE_NO_TITLE);
         d.setContentView(R.layout.custom_dialog_invoice);
         Objects.requireNonNull(d.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         d.show();
+
+// Define widgets
+        txtProduct = d.findViewById(R.id.txt_product_name);
+        editQty = d.findViewById(R.id.edit_change_qty);
+        editPrice = d.findViewById(R.id.edit_change_price);
+        editDiscount = d.findViewById(R.id.edit_change_discount);
+        editTotal = d.findViewById(R.id.edit_change_total);
+        btnDone = d.findViewById(R.id.btn_done_change);
+        btnCancel = d.findViewById(R.id.btn_cancel_change);
+
+//    Set values in fields
+        txtProduct.setText(product);
+        editQty.setText(String.valueOf(qty));
+        editPrice.setText(String.valueOf(price));
+        editTotal.setText(String.valueOf(total));
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                d.dismiss();
+            }
+        });
+
+
     }
 }
