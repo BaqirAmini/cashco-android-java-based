0e87ee2185dfdb5eb69e0470c61e54ce94df4d89
= <=>
2019-10-14 16:19:24 +0430

    Generate apk with remote server after customer registeration completed

--- app/src/main/java/com/xamuor/cashco/Utilities/Routes.java
+++ app/src/main/java/com/xamuor/cashco/Utilities/Routes.java
@@ -2,18 +2,18 @@ package com.xamuor.cashco.Utilities;
 
 public class Routes {
 //    Remote-server...
-//    private static String IMG_PATH = "http://cashco.xamuor.com/uploads/";
+    private static String IMG_PATH = "http://cashco.xamuor.com/uploads/";
 
 //    Virtual-device IP
 //    private static String IMG_PATH = "http://192.168.56.1:8000/uploads/";
 //    NEW IP GENERATED
-    private static String IMG_PATH = "http://192.168.232.1:8000/uploads/";
+//    private static String IMG_PATH = "http://192.168.232.1:8000/uploads/";
     public static String setUrl(String route) {
         //    Remote-server...
-//        String url = "http://cashco.xamuor.com/api/" + route;
+        String url = "http://cashco.xamuor.com/api/" + route;
 //        String url = "http://192.168.56.1:8000/api/" + route;
 //        NEW IP GENERATED
-        String url = "http://192.168.232.1:8000/api/" + route;
+//        String url = "http://192.168.232.1:8000/api/" + route;
 //        Virtual-device IP
         return url;
 
